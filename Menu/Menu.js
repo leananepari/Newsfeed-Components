8284bac0927f6523d283d10ad852d59de108f810
Leana <leananeparidze.com>
2019-06-19 16:12:30 -0700

    adds menu slide in and out animation

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,7 +1,28 @@
+let open = false;
 
-const toggleMenu = () => {
+const toggleMenu = (event) => {
   // Toggle the "menu--open" class on your menu refence. 
+  open ? slideOut() : slideIn();
+}
+
+const slideIn = () => {
+  menu.style.animation = 'slideIn 2s'
   menu.classList.toggle('menu--open');
+  open = true;
+  setTimeout(function() {
+    menu.removeAttribute('style');
+  }, 2000)
+}
+
+function slideOut() {
+  menu.style.animation = 'slideOut 2s'
+  console.log(menu)
+  open = false;
+  setTimeout(function() {
+    menu.classList.toggle('menu--open');
+    menu.removeAttribute('style');
+  }, 2000)
+
 }
 
 // Start Here: Create a reference to the ".menu" class
@@ -12,3 +33,4 @@ const menuButton = document.querySelector('.menu-button');
 menuButton.addEventListener('click', (event) => {
   toggleMenu();
 })
+
