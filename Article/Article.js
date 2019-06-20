5596098b1d65a7656afa297bd15933c0613d5987
Leana <leananeparidze.com>
2019-06-19 21:40:20 -0700

    adds read button which removes article on click

--- Article/Article.js
+++ Article/Article.js
@@ -6,17 +6,27 @@ class Article {
     this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
     this.expandButton = domElement.querySelector('.expandButton');
+    this.closeButton = domElement.querySelector('.close-button');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = 'expand';
+    this.expandButton.textContent = 'Click to expand';
     
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle());
+    this.closeButton.addEventListener('click', () => this.closeArticle());
     
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article-open');
+    if (this.expandButton.textContent === 'Click to close') {
+      this.expandButton.textContent = 'Click to expand'
+    } else {
+      this.expandButton.textContent = 'Click to close';
+    }
+  }
+  closeArticle() {
+    this.domElement.remove()
   }
 }
 
