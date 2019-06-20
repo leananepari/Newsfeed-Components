92b19f646ad071309e32b43a20c5031e5daebd72
Leana <leananeparidze.com>
2019-06-19 18:17:40 -0700

    adds bonus slide out

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -4,25 +4,22 @@ const toggleMenu = (event) => {
   // Toggle the "menu--open" class on your menu refence. 
   open ? slideOut() : slideIn();
 }
-
 const slideIn = () => {
-  menu.style.animation = 'slideIn 2s'
+  menu.style.animation = 'slideIn 1s'
   menu.classList.toggle('menu--open');
   open = true;
   setTimeout(function() {
     menu.removeAttribute('style');
-  }, 2000)
+  }, 1000)
 }
 
 function slideOut() {
-  menu.style.animation = 'slideOut 2s'
-  console.log(menu)
+  menu.style.animation = 'slideOut 1s'
   open = false;
   setTimeout(function() {
     menu.classList.toggle('menu--open');
     menu.removeAttribute('style');
-  }, 2000)
-
+  }, 1000)
 }
 
 // Start Here: Create a reference to the ".menu" class
@@ -30,7 +27,14 @@ const menu = document.querySelector('.menu')
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
+const body = document.querySelectorAll('.articles').item(0);
+
 menuButton.addEventListener('click', (event) => {
   toggleMenu();
+})
+body.addEventListener('click', (event) => {
+  if (open === true) {
+    slideOut();
+  }
 })
 
