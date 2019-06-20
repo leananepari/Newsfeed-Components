fdcfd8c1f4f0c188cb1e879e68ecb48bd0f92b34
Leana <leananeparidze.com>
2019-06-19 20:30:03 -0700

    refactors menu slide in and out functionality by using css margin transision. This fixes a bug setTimeout added

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -4,22 +4,15 @@ const toggleMenu = (event) => {
   // Toggle the "menu--open" class on your menu refence. 
   open ? slideOut() : slideIn();
 }
+
 const slideIn = () => {
-  menu.style.animation = 'slideIn 1s'
   menu.classList.toggle('menu--open');
   open = true;
-  setTimeout(function() {
-    menu.removeAttribute('style');
-  }, 1000)
 }
 
-function slideOut() {
-  menu.style.animation = 'slideOut 1s'
+const slideOut = () => {
+  menu.classList.toggle('menu--open');
   open = false;
-  setTimeout(function() {
-    menu.classList.toggle('menu--open');
-    menu.removeAttribute('style');
-  }, 1000)
 }
 
 // Start Here: Create a reference to the ".menu" class
